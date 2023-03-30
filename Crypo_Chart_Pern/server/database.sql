ff3a488a980fffa2e9153e8c7b8b296dd3c85f2e
Niimaii <nimaicgomes@gmail.com>
2023-03-30 17:22:18 -0300

    refactored

--- Crypo_Chart_Pern/server/database.sql
+++ Crypo_Chart_Pern/server/database.sql
@@ -22,38 +22,6 @@ INSERT INTO users(email, password) VALUES ('someEamil', 'somePass');
 -- *** Crypto Databases ***
 
 -- 0 
-
-CREATE TABLE crypto_market (
-  crypto_id TEXT PRIMARY KEY,
-  symbol TEXT,
-  name TEXT,
-  image TEXT,
-  current_price NUMERIC,
-  market_cap NUMERIC,
-  market_cap_rank INTEGER,
-  fully_diluted_valuation NUMERIC,
-  total_volume NUMERIC,
-  volume_24hr NUMERIC,
-  high_24h NUMERIC,
-  low_24h NUMERIC,
-  price_change_24h NUMERIC,
-  price_change_percentage_24h NUMERIC,
-  market_cap_change_24h NUMERIC,
-  market_cap_change_percentage_24h NUMERIC,
-  circulating_supply NUMERIC,
-  total_supply NUMERIC,
-  max_supply NUMERIC
-);
-
-
-CREATE TABLE crypto_chart (
-  crypto_id TEXT,
-  chartDays TEXT,
-  timestamp BIGINT,
-  price NUMERIC,
-  PRIMARY KEY (crypto_id, timestamp, chartDays),
-  unix NUMERIC
-);
 CREATE TABLE crypto_market (
   crypto_id VARCHAR(50) PRIMARY KEY,
   symbol VARCHAR(50),
@@ -73,7 +41,8 @@ CREATE TABLE crypto_market (
   market_cap_change_percentage_24h NUMERIC,
   circulating_supply NUMERIC,
   total_supply NUMERIC,
-  max_supply NUMERIC
+  max_supply NUMERIC,
+  unix NUMERIC
 );
 
 
@@ -83,17 +52,5 @@ CREATE TABLE crypto_chart (
   timestamp BIGINT,
   price NUMERIC,
   PRIMARY KEY (crypto_id, timestamp, chartDays),
-  unix NUMERIC
-);
-
--- TEST
-
--- Tried to fix data being inserted in order and it didn't work
-CREATE TABLE crypto_chart (
-  id SERIAL PRIMARY KEY,
-  crypto_id VARCHAR(50),
-  chartDays VARCHAR(50),
-  timestamp BIGINT,
-  price NUMERIC,
   unix NUMERIC
 );
--- Crypo_Chart_Pern/server/src/hooks/cryptoMarketFetch.js
+++ Crypo_Chart_Pern/server/src/hooks/cryptoMarketFetch.js
@@ -1,33 +1,5 @@
-// How many cryptos do you want to gather
-let cryptoAmount = 100;
-
-// Gets market data for multiple cryptos
-const { data } = await axios(
-  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${cryptoAmount}&page=1&sparkline=false`
-);
-
-// DB MARKET REQUEST
 await db.query(
-  `INSERT INTO crypto_market (crypto_id, symbol, name, image, current_price, market_cap, market_cap_rank, fully_diluted_valuation, total_volume, volume_24hr, high_24h, low_24h, price_change_24h, price_change_percentage_24h, market_cap_change_24h, market_cap_change_percentage_24h, circulating_supply, total_supply, max_supply) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19)
-  ON CONFLICT (crypto_id) DO UPDATE SET
-  symbol = EXCLUDED.symbol,
-  name = EXCLUDED.name,
-  image = EXCLUDED.image,
-  current_price = EXCLUDED.current_price,
-  market_cap = EXCLUDED.market_cap,
-  market_cap_rank = EXCLUDED.market_cap_rank,
-  fully_diluted_valuation = EXCLUDED.fully_diluted_valuation,
-  total_volume = EXCLUDED.total_volume,
-  volume_24hr = EXCLUDED.volume_24hr,
-  high_24h = EXCLUDED.high_24h,
-  low_24h = EXCLUDED.low_24h,
-  price_change_24h = EXCLUDED.price_change_24h,
-  price_change_percentage_24h = EXCLUDED.price_change_percentage_24h,
-  market_cap_change_24h = EXCLUDED.market_cap_change_24h,
-  market_cap_change_percentage_24h = EXCLUDED.market_cap_change_percentage_24h,
-  circulating_supply = EXCLUDED.circulating_supply,
-  total_supply = EXCLUDED.total_supply,
-  max_supply = EXCLUDED.max_supply;`,
+  `INSERT INTO crypto_market (crypto_id, symbol, name, image, current_price, market_cap, market_cap_rank, fully_diluted_valuation, total_volume, volume_24hr, high_24h, low_24h, price_change_24h, price_change_percentage_24h, market_cap_change_24h, market_cap_change_percentage_24h, circulating_supply, total_supply, max_supply, unix) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20) ON CONFLICT (crypto_id) DO UPDATE SET symbol = EXCLUDED.symbol, name = EXCLUDED.name, image = EXCLUDED.image, current_price = EXCLUDED.current_price, market_cap = EXCLUDED.market_cap, market_cap_rank = EXCLUDED.market_cap_rank, fully_diluted_valuation = EXCLUDED.fully_diluted_valuation, total_volume = EXCLUDED.total_volume, volume_24hr = EXCLUDED.volume_24hr, high_24h = EXCLUDED.high_24h, low_24h = EXCLUDED.low_24h, price_change_24h = EXCLUDED.price_change_24h, price_change_percentage_24h = EXCLUDED.price_change_percentage_24h, market_cap_change_24h = EXCLUDED.market_cap_change_24h, market_cap_change_percentage_24h = EXCLUDED.market_cap_change_percentage_24h, circulating_supply = EXCLUDED.circulating_supply, total_supply = EXCLUDED.total_supply, max_supply = EXCLUDED.max_supply, unix = EXCLUDED.unix;`,
   [
     coinID,
     coinMarket.symbol,
@@ -48,5 +20,6 @@ await db.query(
     coinMarket.circulating_supply,
     coinMarket.total_supply,
     coinMarket.max_supply,
+    current,
   ]
 );
