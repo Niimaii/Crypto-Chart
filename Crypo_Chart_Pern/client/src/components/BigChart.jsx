10b92b118a55684a980efef1fb3103f5304280c1
Niimaii <nimaicgomes@gmail.com>
2023-05-24 17:37:47 -0300

    mobile design for the coin page chart

---
+++ Crypo_Chart_Pern/client/src/components/BigChart.jsx
@@ -0,0 +1,154 @@
+/*
+TODO: ADD BUTTON LOGIC
+*/
+
+import useAxios from '../hooks/useAxios';
+import moment from 'moment';
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { TempIcon } from '../icons/icons';
+import { useContext } from 'react';
+import { CryptoContext } from '../context/CryptoContext';
+import useDB from '../hooks/useDB';
+import { useQueryClient } from '@tanstack/react-query';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+);
+
+function BigChart({ chartData, coin }) {
+  const queryClient = useQueryClient();
+  const market = queryClient.getQueryData(['market']);
+
+  //   Locate the correct crypto market data based on the coin of interest
+  const marketData = market.find((crypto) => crypto.crypto_id === coin);
+
+  // Currency formatter
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
+  //   ↓↓↓↓↓↓↓↓↓↓↓↓↓↓ Chart Setup ↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+
+  // Bracket notation
+  const cryptoData = chartData.map((value) => {
+    return {
+      x: value[0],
+      y: value[1],
+    };
+  });
+
+  const options = {
+    // Makes the chart adjust to the div size
+    responsive: true,
+    // Change aspect Ratio
+    aspectRatio: 2 / 1,
+    scales: {
+      x: {
+        grid: {
+          // Removes grid lines
+          display: false,
+        },
+        border: {
+          // This removes the outside border
+          display: false,
+        },
+        // Removes Visual Data
+        ticks: {
+          display: false,
+        },
+      },
+      y: {
+        grid: {
+          // Removes grid lines
+          display: false,
+        },
+        border: {
+          // This removes the outside border
+          display: false,
+        },
+        // Removes Visual Data
+        ticks: {
+          display: false,
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        // Removes title
+        display: false,
+      },
+    },
+  };
+
+  const data = {
+    labels: cryptoData.map((value) => moment(value.x).format('MMMDD')),
+    datasets: [
+      {
+        // Line Width
+        borderWidth: 1.5,
+        fill: true,
+        data: cryptoData.map((value) => value.y),
+        borderColor: 'rgb(127, 195, 121)',
+        backgroundColor: 'rgb(127, 195, 121, 0.05)',
+        // Smoothens line
+        tension: 0.4,
+        // Removes dots
+        pointRadius: 0,
+      },
+    ],
+  };
+
+  //   ↑↑↑↑↑↑↑↑↑↑↑↑↑↑ Chart Setup ↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+
+  return (
+    <div>
+      <div className='flex justify-between'>
+        <div className='flex items-center m-0'>
+          <img
+            className='h-12 mr-3'
+            src={marketData.image}
+            alt={marketData.name}
+          />
+
+          <div>
+            <h1 className='text-base m-0 h-5 font-medium'>{marketData.name}</h1>
+            <h1 className='text-base m-0 font-extralight'>
+              {marketData.symbol.toUpperCase()}
+            </h1>
+          </div>
+        </div>
+        <div className='flex gap-3'>
+          <p>1D</p>
+          <p>1M</p>
+          <p>1Y</p>
+        </div>
+      </div>
+      <h1>{formatter.format(marketData.current_price)}</h1>
+      <div className='h-80'>
+        <Line options={options} data={data} />
+      </div>
+    </div>
+  );
+}
+
+export default BigChart;
--- Crypo_Chart_Pern/client/src/routes/CoinPage.jsx
+++ Crypo_Chart_Pern/client/src/routes/CoinPage.jsx
@@ -2,15 +2,19 @@ import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { useContext } from 'react';
 import { CryptoContext } from '../context/CryptoContext';
 import { getChart } from '../api/cryptoAPI';
+import BigChart from '../components/BigChart';
+import { useParams } from 'react-router-dom';
 
 function CoinPage() {
   const { portfolio, market } = useContext(CryptoContext);
   const { data, isLoading } = useQuery({
-    queryKey: ['market'],
+    queryKey: ['coin'],
     queryFn: () => getChart('bitcoin', 30),
     staleTime: 1000 * 60 * 3,
     refetchInterval: 1000 * 60 * 3,
   });
+
+  const { coin } = useParams();
 
   if (portfolio.isLoading || isLoading || market.isLoading) {
     return <h1>Loading...</h1>;
@@ -18,7 +22,11 @@ function CoinPage() {
 
   const chartData = data.data.chart;
 
-  return <div>hey</div>;
+  return (
+    <div>
+      <BigChart chartData={chartData} coin={coin} />
+    </div>
+  );
 }
 
 export default CoinPage;
