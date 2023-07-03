2d710fc15756297a190b7bd1dde7873743c21da1
Niimaii <nimaicgomes@gmail.com>
2023-07-03 15:52:18 -0300

    added options when pressing the hamburgger menu

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,24 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import vscodium from '../assets/vscodium.png';
-import { Hamburger, SearchIcon, UserIcon } from '../icons/icons';
+import {
+  AboutIcon,
+  Hamburger,
+  LogoutIcon,
+  PieChartIcon,
+  PortfolioIcon,
+  RightToggleArrow,
+  SearchIcon,
+  SettingsIcon,
+  UserIcon,
+} from '../icons/icons';
 import { CryptoContext } from '../context/CryptoContext';
 import { fetchProtectedInfo, onLogout } from '../api/authAPI';
 import SearchBar from './SearchBar';
 
 function Navbar() {
   const { isAuth, unAuthenticateUser } = useContext(CryptoContext);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const logout = async () => {
     try {
@@ -21,6 +32,18 @@ function Navbar() {
       console.log(err.response);
     }
   };
+
+  useEffect(() => {
+    const dropdownElement = document.getElementById('dropdown');
+
+    if (isDropdownOpen) {
+      dropdownElement.style.height = '200px';
+      dropdownElement.style.padding = '15px';
+    } else {
+      dropdownElement.style.height = '0px';
+      dropdownElement.style.padding = '0px';
+    }
+  }, [isDropdownOpen]);
 
   return (
     <div className='nav_bg'>
@@ -39,9 +62,50 @@ function Navbar() {
 
             <SearchBar />
           </div>
-          <button className='hamburger'>
+          <button
+            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+            className='hamburger'
+          >
             <Hamburger />
           </button>
+          <article id='dropdown' className='nav_dropdown'>
+            <div className='nav_dropdown_pages'>
+              <NavLink
+                onClick={() => setIsDropdownOpen(false)}
+                className='nav_dropdown_button'
+                to='/portfolio'
+              >
+                <PortfolioIcon />
+                <p>Portfolio</p>
+              </NavLink>
+
+              <NavLink
+                onClick={() => setIsDropdownOpen(false)}
+                to='/settings'
+                className='nav_dropdown_settings nav_dropdown_button'
+              >
+                <div>
+                  <SettingsIcon />
+                </div>
+                <p>Settings</p>
+              </NavLink>
+              <NavLink
+                onClick={() => setIsDropdownOpen(false)}
+                className='nav_dropdown_button'
+                to='/about'
+              >
+                <AboutIcon />
+                <p>About</p>
+              </NavLink>
+            </div>
+            <hr />
+            <div className='nav_dropdown_bottom'>
+              <button onClick={logout} className='nav_dropdown_button'>
+                <LogoutIcon />
+                <p>Logout</p>
+              </button>
+            </div>
+          </article>
           <div className='flex justify-between w-full'>
             <div className='nav_response w-full flex justify-center items-center'>
               <div className='nav_options w-60 flex items-center justify-between'>
