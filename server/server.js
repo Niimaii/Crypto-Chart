82d7e16ff66362ea6f94c9e4e826e41897d134d6
Niimaii <nimaicgomes@gmail.com>
2023-07-08 20:36:12 -0300

    .env fix

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 // This connects to the index.js file in src and automatically runs the file. If it didn't, then the serer wouldn't start
 const { config } = require('dotenv');
-config();
+const { join } = require('path');
+config({ path: join(__dirname, '.env') });
 const src = require('./src/index');
