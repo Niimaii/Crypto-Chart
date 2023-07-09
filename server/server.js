64cdbbd5048ea6e98f9b9e771697e501f0dd0d3b
Niimaii <nimaicgomes@gmail.com>
2023-07-08 21:04:03 -0300

    reducing package size

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 // This connects to the index.js file in src and automatically runs the file. If it didn't, then the serer wouldn't start
 const { config } = require('dotenv');
+// This join IS SO FUCKING IMPORTANT. We setup the path for the .env file by default, this way in production it's found.
 const { join } = require('path');
 config({ path: join(__dirname, '.env') });
 const src = require('./src/index');
