febd70a04ffec171f4f7a7801eb3f7451d8f0a58
Niimaii <nimaicgomes@gmail.com>
2023-07-11 01:06:32 -0300

    caoursel no longer is dumb on ultra wide screen

--- client/src/components/CryptoCarousel.tsx
+++ client/src/components/CryptoCarousel.tsx
@@ -14,48 +14,52 @@ function CryptoCarousel() {
   const cachedMarket = queryClient.getQueryData(['market']);
 
   // This is duct tape and fucking glue to get ChartJS to actually loop without issues
-  const loop = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  const loop = [1, 2, 3, 4, 5, 6, 0];
 
   return (
-    <Swiper
-      centeredSlides={true}
-      // centerInsufficientSlides={true}
-      loop={true}
-      // slidesOffsetAfter={30}
-      // slidesOffsetBefore={30}
-      //   Changes how many slides can be seen based on screen size
-      breakpoints={{
-        '@0.00': {
-          slidesPerView: 1,
-          spaceBetween: 1,
-        },
-        '@0.75': {
-          slidesPerView: 1,
-          spaceBetween: 1,
-        },
-        '@1.00': {
-          slidesPerView: 3,
-          spaceBetween: 0.5,
-        },
-        '@1.50': {
-          slidesPerView: 3,
-          spaceBetween: 0.5,
-        },
-      }}
-    >
-      {cachedMarket &&
-        loop.map((i, index) => {
-          let coin = cachedMarket[i];
-          return (
-            // You need to add SwiperSlide for Swiper to work properly
-            <SwiperSlide key={coin.crypto_id}>
-              <NavLink to={`/${coin.crypto_id}`}>
-                <SmallChart coin={coin} key={coin.crypto_id + index} />
-              </NavLink>
-            </SwiperSlide>
-          );
-        })}
-    </Swiper>
+    <div className='swiper_container'>
+      <div className='swiper_carousel'>
+        <Swiper
+          centeredSlides={true}
+          // centerInsufficientSlides={true}
+          loop={true}
+          // slidesOffsetAfter={30}
+          // slidesOffsetBefore={30}
+          //   Changes how many slides can be seen based on screen size
+          breakpoints={{
+            '@0.00': {
+              slidesPerView: 1,
+              spaceBetween: 1,
+            },
+            '@0.75': {
+              slidesPerView: 1,
+              spaceBetween: 1,
+            },
+            '@1.00': {
+              slidesPerView: 3,
+              spaceBetween: 0.5,
+            },
+            '@1.50': {
+              slidesPerView: 3,
+              spaceBetween: 0.5,
+            },
+          }}
+        >
+          {cachedMarket &&
+            loop.map((i, index) => {
+              let coin = cachedMarket[i];
+              return (
+                // You need to add SwiperSlide for Swiper to work properly
+                <SwiperSlide key={coin.crypto_id}>
+                  <NavLink to={`/${coin.crypto_id}`}>
+                    <SmallChart coin={coin} key={coin.crypto_id + index} />
+                  </NavLink>
+                </SwiperSlide>
+              );
+            })}
+        </Swiper>
+      </div>
+    </div>
   );
 }
 
